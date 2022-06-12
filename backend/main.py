c8935b01da683ac5bb45e4065895908316f91b66
Matvey Semenov <matveytri777@gmail.com>
2022-06-12 19:57:44 +0300

    added router user at main file

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from perfume.routes import router as perfume_routes
+from user.routes import router as user_routes
+
 
 app = FastAPI()
 app.include_router(perfume_routes)
+app.include_router(user_routes)
 app.mount("/static", StaticFiles(directory="static"), name="static")
