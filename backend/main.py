e1d5655f70eccefb04b5eb552b3a5851f464c831
Matvey Semenov <matveytri777@gmail.com>
2022-06-12 20:35:16 +0300

    temporarily commended static and added if name == main

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
+import uvicorn
+
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+# from fastapi.staticfiles import StaticFiles
 
 from perfume.routes import router as perfume_routes
 from user.routes import router as user_routes
@@ -8,4 +10,8 @@ from user.routes import router as user_routes
 app = FastAPI()
 app.include_router(perfume_routes)
 app.include_router(user_routes)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host='0.0.0.0')
