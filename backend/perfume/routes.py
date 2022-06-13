1fa5613c8693bcaf67fc96c1fe6995da2d62f7e5
Matvey Semenov <matveytri777@gmail.com>
2022-06-13 04:17:00 +0300

    deleted logic from routes

--- backend/perfume/routes.py
+++ backend/perfume/routes.py
@@ -17,7 +17,6 @@ async def get_perfume_by_brand(
     brand: str = Path(..., title='The perfume\'s brand'),
     limit: Union[int, None] = Query(None, title='Limit of perfumes'),
 ):
-    brand = brand.replace('_', ' ').replace('-', ' ').title()
     perfumes = CRUD.get_perfumes_by_brand(brand, limit)
 
     # Nothing to show
